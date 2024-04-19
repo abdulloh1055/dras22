ef71f89a1dda49fe712ffbc95247b7328468ebd3
Abdulloh Ahmadaliyev <abdulloh.dev.843@gmail.com>
2024-04-19 11:48:21 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,9 @@
+let ism = prompt("ismingizni kriting"); // husan
+let harf = prompt("birorta harf kriting"); // t
+let orni = ism.indexOf(harf); // -1
+
+if (orni === -1) {
+    alert("Bu " + ism + " ismda " + harf + " mavjud emas");
+} else {
+    aler("Bu " + ism + "ismda " + harf + "mavjud")
+}
